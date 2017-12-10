30f182c762b3b4ea6484035b431310b3700b8d3d
Satrya Rangga <system@techbro.id>
2017-12-10 23:13:22 +0700

    customer detail

--- resources/views/order/order-partner/detail.blade.php
+++ resources/views/order/order-partner/detail.blade.php
@@ -33,11 +33,12 @@
         <div class="col-sm-6 invoice-col">
           <b style="font-size: 18px">Customer Detail</b>
           <address>
-            <strong>John Doe</strong><br>
-            795 Folsom Ave, Suite 600<br>
-            San Francisco, CA 94107<br>
-            Phone: (555) 539-1037<br>
-            Email: john.doe@example.com
+            <strong>{{$customer->first_name}} {{$customer->last_name}}</strong><br>
+            {{$delivery->to_address}}<br>
+            {{$delivery->to_city_name}}, {{$delivery->to_province_name}}<br>
+            Postcode: {{$delivery->to_postcode}}<br>
+            Phone: {{$delivery->to_phone}}<br>
+            Email: {{$customer->email}}
           </address>
         </div>
       </div>
