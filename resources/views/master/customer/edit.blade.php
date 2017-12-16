495baebe21cd6f93c581b0e0dea9fed6eb54555d
Satrya Rangga <system@techbro.id>
2017-12-16 16:12:52 +0700

    edit password customer

--- resources/views/master/customer/edit.blade.php
+++ resources/views/master/customer/edit.blade.php
@@ -22,52 +22,66 @@
 	              <div class="box-body">
 	                
 	                <div class="form-group">
-	                  <label for="first_name" class="col-sm-2 control-label">First Name</label>
-	                  <div class="col-sm-10">
+	                  <label for="first_name" class="col-sm-3 control-label">First Name</label>
+	                  <div class="col-sm-8">
 	                    <input type="text" class="form-control" name="first_name" value="{{$row->first_name}}" id="first_name" placeholder="First Name">
 	                  </div>
 	                </div>
 	                
 	                <div class="form-group">
-	                  <label for="last_name" class="col-sm-2 control-label">Last Name</label>
-	                  <div class="col-sm-10">
+	                  <label for="last_name" class="col-sm-3 control-label">Last Name</label>
+	                  <div class="col-sm-8">
 	                  	<input type="text" class="form-control" name="last_name" value="{{$row->last_name}}" id="last_name" placeholder="Last Name">
 	                  </div>
 	                </div>
 
 	                <div class="form-group">
-			               <label for="gender" class="col-sm-2 control-label">Gender</label>
-			               <div class="col-sm-10">
+	                  <label for="last_name" class="col-sm-3 control-label">Last Name</label>
+	                  <div class="col-sm-8">
+	                  	<input type="text" class="form-control" name="last_name" value="{{$row->last_name}}" id="last_name" placeholder="Last Name">
+	                  </div>
+	                </div>
+
+	                <div class="form-group">
+	                  <label for="file" class="col-sm-3 control-label">*Fill password if you want to change</label>
+	                  <div class="col-sm-8">
+	                    <input type="password" class="form-control" name="password" id="pass">
+	                  </div>
+	                </div>
+
+	                <div class="form-group">
+			               <label for="gender" class="col-sm-3 control-label">Gender</label>
+			               <div class="col-sm-8">
 				               <label class="radio-inline"><input @if($row->gender == 1) checked="checked" @endif type="radio" value="1" name="gender">Male</label>
 		                  	   <label class="radio-inline"><input @if($row->gender == 2) checked="checked" @endif type="radio" value="2" name="gender">Female</label>
 			               </div>
 			         </div> 
 
 	                <div class="form-group">
-	                  <label for="phone" class="col-sm-2 control-label">Phone Number</label>
-	                  <div class="col-sm-10">
+	                  <label for="phone" class="col-sm-3 control-label">Phone Number</label>
+	                  <div class="col-sm-8">
 	                  	<input type="text" class="form-control" name="phone" value="{{$row->phone}}" id="phone" placeholder="Phone Number">
 	                  </div>
 	                </div>
 
 	                <div class="form-group">
-	                  <label for="email" class="col-sm-2 control-label">Email</label>
-	                  <div class="col-sm-10">
+	                  <label for="email" class="col-sm-3 control-label">Email</label>
+	                  <div class="col-sm-8">
 	                  	<input type="email" class="form-control" name="email" readonly value="{{$row->email}}" id="email" placeholder="Email Address">
 	                  </div>
 	                </div>
 
 	                <div class="form-group">
-	                  <label for="province" class="col-sm-2 control-label">Province</label>
-	                  <div class="col-sm-10">
+	                  <label for="province" class="col-sm-3 control-label">Province</label>
+	                  <div class="col-sm-8">
 	                  	<input type="province" class="form-control" name="province" value="{{getFieldOfTable('provinces', $row->addr_province_id, 'name')}}" id="province" placeholder="Province Address">
 	                  	<input type="hidden" name="province_id" id="province_id" value="{{$row->addr_province_id}}">
 	                  </div>
 	                </div>
 
 	                <div class="form-group">
-	                  <label for="city" class="col-sm-2 control-label">City</label>
-	                  <div class="col-sm-10">
+	                  <label for="city" class="col-sm-3 control-label">City</label>
+	                  <div class="col-sm-8">
 	                  	<select class="form-control" id="city_id" name="city_id">
 	                  		<option>Choose City</option>
 	                  	</select>
@@ -75,22 +89,22 @@
 	                </div>
 
 	                <div class="form-group">
-	                  <label for="address" class="col-sm-2 control-label">Address</label>
-	                  <div class="col-sm-10">
+	                  <label for="address" class="col-sm-3 control-label">Address</label>
+	                  <div class="col-sm-8">
 	                  	<textarea rows="5" name="addr_street" style="width:100%">{{$row->addr_street}}</textarea>
 	                  </div>
 	                </div>
 
 	                <div class="form-group">
-	                  <label for="zipcode" class="col-sm-2 control-label">Zipcode</label>
-	                  <div class="col-sm-10">
+	                  <label for="zipcode" class="col-sm-3 control-label">Zipcode</label>
+	                  <div class="col-sm-8">
 	                  	<input type="text" class="form-control" name="zipcode" value="{{$row->addr_zipcode}}" id="zipcode" placeholder="Zipcode">
 	                  </div>
 	                </div>
 
 	                <div class="form-group">
-	                  <label for="birthdate" class="col-sm-2 control-label">Birthdate</label>
-	                  <div class="col-sm-10">
+	                  <label for="birthdate" class="col-sm-3 control-label">Birthdate</label>
+	                  <div class="col-sm-8">
 	                  	<input type="text" class="form-control datepicker" name="birthdate" value="{{$row->birthdate}}" id="birthdate" placeholder="Birthdate">
 	                  </div>
 	                </div>
